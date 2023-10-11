bb7df1c69455f0e7a96816eb5138a28c8f5a1a4f
michelantoniovieira <michelantonio_gti@hotmail.com>
2023-10-11 07:09:28 -0300

    v.0.0.1.1

---
+++ src/main/java/br/mic/financiamento_coletivo/controller/PagamentoRifaController.java
@@ -0,0 +1,83 @@
+package br.mic.financiamento_coletivo.controller;
+
+import com.mercadopago.MercadoPagoConfig;
+import com.mercadopago.client.common.IdentificationRequest;
+import com.mercadopago.client.payment.PaymentClient;
+import com.mercadopago.client.payment.PaymentCreateRequest;
+import com.mercadopago.client.payment.PaymentPayerRequest;
+import com.mercadopago.exceptions.MPApiException;
+import com.mercadopago.exceptions.MPException;
+import com.mercadopago.resources.payment.Payment;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+
+@Controller
+public class PagamentoRifaController {
+
+    @GetMapping("/pagamentoRifa")
+    public String paginaPagamentoRifa() {
+        return "pagamentoRifa";
+    }
+
+    @PostMapping("/process_payment")
+    public String process_payment(Model model, HttpServletRequest request) {
+        try {
+            // Configure a chave de acesso do MercadoPago (substitua pela sua chave real)
+            MercadoPagoConfig.setAccessToken("APP_USR-5579030860678637-101022-182436c107f980c52a171e0f27fd930c-202843783");
+
+            PaymentClient client = new PaymentClient();
+
+            // Obtenha os dados do formulário
+            String payerFirstName = request.getParameter("payerFirstName");
+            String payerLastName = request.getParameter("payerLastName");
+            String email = request.getParameter("email");
+
+            // Crie a solicitação de pagamento usando os dados do formulário
+            PaymentCreateRequest paymentCreateRequest = PaymentCreateRequest.builder()
+                    .transactionAmount(new BigDecimal("100"))
+                    .description("Título do produto")
+                    .paymentMethodId("pix")
+                    .dateOfExpiration(OffsetDateTime.of(2023, 10, 12, 10, 10, 10, 0, ZoneOffset.UTC))
+                    .payer(
+                            PaymentPayerRequest.builder()
+                                    .email(email)
+                                    .firstName(payerFirstName)
+                                    .lastName(payerLastName)
+                                    .identification(
+                                            IdentificationRequest.builder().type("CPF").number("19119119100").build())
+                                    .build())
+                    .build();
+
+            // Crie o pagamento
+            Payment payment = client.create(paymentCreateRequest);
+
+            // Obtenha a URL do Pix e o código QR
+            String pixUrl = payment.getPointOfInteraction().getTransactionData().getQrCode();
+            String qrCodeBase64 = payment.getPointOfInteraction().getTransactionData().getQrCodeBase64();
+
+            // Agora você pode usar pixUrl e qrCodeBase64 para exibir o Pix ao cliente
+            model.addAttribute("pixUrl", pixUrl);
+            model.addAttribute("qrCodeBase64", qrCodeBase64);
+
+            System.out.println(pixUrl);
+
+
+        } catch (MPApiException ex) {
+            System.out.printf(
+                    "MercadoPago Error. Status: %s, Content: %s%n",
+                    ex.getApiResponse().getStatusCode(), ex.getApiResponse().getContent());
+            // Lide com o erro de API do MercadoPago, como registrar ou fornecer feedback ao usuário.
+        } catch (MPException ex) {
+            ex.printStackTrace();
+            // Lide com outras exceções MPException, se necessário.
+        }
+        return "resultadoPagamento"; // Redirecione para uma página de resultado
+    }
+}
---
+++ src/main/java/br/mic/financiamento_coletivo/model/Jogo.java
@@ -0,0 +1,107 @@
+package br.mic.financiamento_coletivo.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+
+public class Jogo
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id_jogo;
+    @Column(name="fk_id_rifa")
+    private int fk_id_rifa;
+    @Column(name="numero_escolhido")
+    private int numero_escolhido;
+    @Column(name="nome_participante")
+    private String nome_participante;
+    @Column(name="sobrenome")
+    private String sobrenome;
+    @Column(name="email")
+    private String email;
+    @Column(name="telefone")
+    private String telefone;
+
+    public Jogo(int fk_id_rifa, int numero_escolhido, String nome_participante, String sobrenome, String email, String telefone)
+    {
+        this.fk_id_rifa = fk_id_rifa;
+        this.numero_escolhido = numero_escolhido;
+        this.nome_participante = nome_participante;
+        this.sobrenome = sobrenome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    public int getId_jogo()
+    {
+        return id_jogo;
+    }
+
+    public void setId_jogo(int id_jogo)
+    {
+        this.id_jogo = id_jogo;
+    }
+
+    public int getFk_id_rifa()
+    {
+        return fk_id_rifa;
+    }
+
+    public void setFk_id_rifa(int fk_id_rifa)
+    {
+        this.fk_id_rifa = fk_id_rifa;
+    }
+
+    public int getNumero_escolhido()
+    {
+        return numero_escolhido;
+    }
+
+    public void setNumero_escolhido(int numero_escolhido)
+    {
+        this.numero_escolhido = numero_escolhido;
+    }
+
+    public String getNome_participante()
+    {
+        return nome_participante;
+    }
+
+    public void setNome_participante(String nome_participante)
+    {
+        this.nome_participante = nome_participante;
+    }
+
+    public String getSobrenome()
+    {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome)
+    {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    public String getTelefone()
+    {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone)
+    {
+        this.telefone = telefone;
+    }
+}
